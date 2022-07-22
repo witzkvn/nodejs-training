0f366b237d09d2b75067aaef7bb391b143333dd9
witzkvn <witz.kevin05@gmail.com>
2022-07-22 20:40:05 +0200

    added html static pages with  css

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -7,12 +8,15 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(path.join(__dirname, "views", "404.html")));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post('/product', (req, res, next) => {
+router.post("/product", (req, res, next) => {
   console.log(req.body);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
